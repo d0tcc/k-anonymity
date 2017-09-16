a4584b2030d7b76b57db67ea2ac28be8c1283f28
Julian Schrittwieser <schrittwieser@me.com>
2017-09-16 13:41:47 +0200

    implemented anonymize_age()

--- Person.py
+++ Person.py
@@ -1,10 +1,18 @@
 class Person():
-	def __init__(self, name, age, sex, zipcode, illness):
-		self.name = name
-		self.age = age
-		self.sex = sex
-		self.zipcode = zipcode
-		self.illness = illness
+    def __init__(self, name, age, sex, zipcode, illness):
+        self.name = name
+        self.age = (int(age), int(age))
+        self.sex = sex
+        self.zipcode = zipcode
+        self.illness = illness
 
-	def __str__(self):
-		return "{0}, {1}, {2}, {3}, {4}".format(self.name, self.age, self.sex, self.zipcode, self.illness)
+    def __str__(self):
+        return "{0}, {1}, {2}, {3}, {4}".format(self.name, self.print_age(), self.sex, self.zipcode, self.illness)
+
+    def print_age(self):
+        from_age = self.age[0]
+        to_age = self.age[1]
+        if from_age == to_age:
+            return str(from_age)
+        else:
+            return "{0} bis {1}".format(from_age, to_age)
--- anonymizer.py
+++ anonymizer.py
@@ -21,14 +21,29 @@ def anonymize_ZIP(persons):
     # p.zipcode = zipcode.replace(index-1,'*')
     return persons
 
-def anonymize_age():
-    # TODO
-    pass
+def anonymize_age(persons):
+    for p in persons:
+        from_age = p.age[0]
+        to_age = p.age[1]
+        age_range = to_age - from_age + 1
+        modulo = from_age % (age_range*2)
+
+        if modulo < age_range:
+            new_from_age = from_age
+            new_to_age = to_age + age_range
+        else:
+            new_from_age = from_age - age_range
+            new_to_age = to_age
+
+        p.age = (new_from_age, new_to_age)
+    return persons
 
 
 def main():
     persons = read_csv()
     persons = anonymize_ZIP(persons)
+    for i in range(3):
+        persons = anonymize_age(persons)
     for p in persons:
         print p
 
