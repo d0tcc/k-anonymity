1650ce72e4b46abe66babbe1f7011f2643ffd58e
Julian Schrittwieser <schrittwieser@me.com>
2017-09-16 12:28:32 +0200

    anonymization of zip code tested

--- anonymizer.py
+++ anonymizer.py
@@ -1,29 +1,37 @@
 import csv
 from Person import Person
 
-def anonymize_ZIP():
-	for p in persons:
-		p.zipcode = p.zipcode[:-1]
-		# zipcode = p.zipcode
-		# index = zipcode.find('*')
-		# p.zipcode = zipcode.replace(index-1,'*')
+
+def read_csv():
+    persons = []
+    with open('data.csv', 'rb') as f:
+        reader = csv.reader(f)
+        raw_persons = list(reader)
+    del raw_persons[0]
+    for p in raw_persons:
+        persons.append(Person(p[0], p[1], p[2], p[3], p[4]))
+    return persons
+
+
+def anonymize_ZIP(persons):
+    for p in persons:
+        p.zipcode = p.zipcode[:-1]
+    # zipcode = p.zipcode
+    # index = zipcode.find('*')
+    # p.zipcode = zipcode.replace(index-1,'*')
+    return persons
 
 def anonymize_age():
-	#TODO
-	pass
+    # TODO
+    pass
+
 
 def main():
-	with open('data.csv', 'rb') as f:
-	    reader = csv.reader(f)
-	    raw_persons = list(reader)
+    persons = read_csv()
+    persons = anonymize_ZIP(persons)
+    for p in persons:
+        print p
 
-	del raw_persons[0]
-	persons = []
-	for p in raw_persons:
-		persons.append(Person(p[0], p[1], p[2], p[3], p[4]))
-
-	for p in persons:
-		print p
 
 if __name__ == "__main__":
-	main()
+    main()
